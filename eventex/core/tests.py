a9d17232bb313900d2a73aa5e02d71309a6a1287
j.moreno <joaomorenocruzrosa@gmail.com>
2023-07-29 15:22:35 -0300

    habilita inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -9,11 +9,11 @@ class HomeTest(TestCase):
         """GET/ must return status code 200"""
         self.assertEqual(200, self.response.status_code)
 
-
     def test_template(self):
         """Must use index.html"""
         self.assertTemplateUsed(self.response, 'index.html')
-
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
 
 
 
