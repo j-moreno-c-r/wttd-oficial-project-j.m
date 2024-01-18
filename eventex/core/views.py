ba067a4f8c5fafeebbe3a61b1a5ba99c96b271b9
j.moreno <joaomorenocruzrosa@gmail.com>
2024-01-18 12:35:05 -0300

    Course como Abstract Base Class

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
-from eventex.core.models import Speaker, Talk
+from eventex.core.models import Speaker, Talk, Course
 
 
 def home(request):
@@ -9,12 +9,12 @@ def home(request):
 
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
-    speaker = Speaker.objects.get(slug=slug)
     return render(request, 'core/speaker_detail.html', {'speaker': speaker})
 
 def talk_list(request):
     context = {
         'morning_talks': Talk.objects.at_morning(),
         'afternoon_talks': Talk.objects.at_afternoon(),
+        'courses': Course.objects.all(),
     }
     return render(request, 'core/talk_list.html', context)
